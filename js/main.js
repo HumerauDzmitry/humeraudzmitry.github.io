dcb94a0a65e7cd5a5f22d8fd9c7b9bec6b498217
HumerauDzmitry <57639610+HumerauDzmitry@users.noreply.github.com>
2020-03-12 22:06:09 +0300

    added js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $('.eng-click').click(function () {
+        document.getElementById("full-name").innerHTML = "Humerau Dzmitry";
+    });
+    $('.ru-click').click(function () {
+        document.getElementById("full-name").innerHTML = "Гумеров Дмитрий";
+    });
+})
